7d735fad143cf85220cafec903b8ec372be1a59f
Ted Stresen-Reuter <ted@secret-source.eu>
2023-01-26 16:09:53 +0000

    Feature Complete: filtering list
    Users can now filter the list of tasks by typing some text in the search field.

--- client/src/app/tickets/tickets.spec.tsx
+++ client/src/app/tickets/tickets.spec.tsx
@@ -1,4 +1,5 @@
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Tickets from './tickets';
 import { BrowserRouter } from 'react-router-dom';
 import fetch from 'jest-fetch-mock';
@@ -26,7 +27,37 @@ describe('Tickets', () => {
   it('should render successfully', () => {
     fetch
       .once(JSON.stringify(tickets));
-    const { baseElement } = render(<BrowserRouter><Tickets tickets={tickets} users={users} /></BrowserRouter>);
+    const { baseElement } = render(<BrowserRouter><Tickets tickets={tickets} users={users} setTickets={jest.fn()} /></BrowserRouter>);
     expect(baseElement).toBeTruthy();
+  });
+});
+
+describe('FilterTickets', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<BrowserRouter><Tickets tickets={tickets} users={users} setTickets={jest.fn()} /></BrowserRouter>);
+    expect(baseElement).toBeTruthy();
+  });
+
+  it('should render a form', () => {
+    const { getByTestId } = render(<BrowserRouter><Tickets tickets={tickets} users={users} setTickets={jest.fn()} /></BrowserRouter>);
+    const form = getByTestId('filter-tickets-form');
+    expect(form).toBeTruthy();
+  });
+
+  it('should render a description input', () => {
+    const { getByLabelText } = render(<BrowserRouter><Tickets tickets={tickets} users={users} setTickets={jest.fn()} /></BrowserRouter>);
+    const descriptionInput = getByLabelText('filter');
+    expect(descriptionInput).toBeTruthy();
+  });
+
+  it('should reduce the number of tickets to those that contain the search query', async () => {
+    const { getByLabelText, getByText, queryByText } = render(<BrowserRouter><Tickets tickets={tickets} users={users} setTickets={jest.fn()} /></BrowserRouter>);
+    const descriptionInput = getByLabelText('filter');
+    userEvent.type(descriptionInput, 'desk');
+    await waitFor(() => {
+      expect(getByText('Move the desk to the new location')).toBeTruthy();
+      expect(queryByText('Install a monitor arm')).toBeNull();
+      expect(queryByText('Install a new mouse')).toBeNull();
+    });
   });
 });
--- client/src/app/tickets/tickets.tsx
+++ client/src/app/tickets/tickets.tsx
@@ -2,6 +2,7 @@ import { Ticket, User } from '@acme/shared-models';
 import { Link } from 'react-router-dom';
 import styles from './tickets.module.css';
 import AddTicketForm from '../add-ticket-form/add-ticket-form';
+import { useState } from 'react';
 
 export interface TicketsProps {
   tickets: Ticket[];
@@ -10,8 +11,12 @@ export interface TicketsProps {
 }
 
 export function Tickets(props: TicketsProps) {
+  const [filteredTickets, setFilteredTickets] = useState(props.tickets);
+  const [filter, setFilter] = useState('');
 
   const toggleTicketStatus = (ticket: Ticket, event: React.MouseEvent<HTMLElement>) => {
+    let originalIcon = event.currentTarget.innerHTML;
+    event.currentTarget.innerHTML = '⏳';
     const METHOD = ticket.completed ? 'DELETE' : 'PUT';
     fetch(`/api/tickets/${ticket.id}/complete`, {
       method: METHOD,
@@ -30,6 +35,7 @@ export function Tickets(props: TicketsProps) {
         });
         props.setTickets(newTickets);
       } else {
+        event.currentTarget.innerHTML = originalIcon;
         throw new Error('Something went wrong');
       }
     });
@@ -40,20 +46,43 @@ export function Tickets(props: TicketsProps) {
     return user ? user.name : 'Unassigned';
   };
 
+  const filterTickets = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const filteredTickets = props.tickets.filter((t) => t.description.includes(e.target.value));
+    setFilteredTickets(filteredTickets);
+    setFilter(e.target.value);
+  };
+
+  const ticketsRenderer = () => {
+    let ticketsArray = []
+    if (filteredTickets.length === 0 && filter === '') {
+      ticketsArray = props.tickets;
+    } else {
+      ticketsArray = filteredTickets;
+    }
+    
+    return ticketsArray.map((t) => (
+      <li key={t.id} className="flex justify-between gap-2">
+        <span className="justify-self-start" onClick={(e) => toggleTicketStatus(t, e)}>{t.completed ? '✅' : '⭕️'}</span>
+        <Link className="grow self-start" to={`/tickets/${t.id}`}>{t.description}</Link>
+        <Link className="" to={`/users/${t.assigneeId}`}>{getUsernameFromId(t.assigneeId)}</Link>
+        <Link to={`/tickets/${t.id}/assign`} className="">Assign</Link>
+      </li>
+    ));
+  };
+
   return (
     <div className={styles['tickets']}>
       <AddTicketForm users={props.users} tickets={props.tickets} setTickets={props.setTickets} />
       <h2 className="border-b-2 border-black mb-4">Tickets</h2>
-      {props.tickets ? (
+
+      <form className="" data-testid="filter-tickets-form">
+        <label htmlFor="filter"><span role="img" aria-label="filter">🔍</span></label>
+        <input className="border-2 m-2" id="filter" name="filter" onChange={filterTickets} autoComplete="off" />
+      </form>
+
+      {filteredTickets ? (
         <ul className="list-none">
-          {props.tickets.map((t) => (
-            <li key={t.id} className="flex justify-between gap-2">
-              <span className="justify-self-start" onClick={(e) => toggleTicketStatus(t, e)}>{t.completed ? '✅' : '⭕️'}</span>
-              <Link className="grow self-start" to={`/tickets/${t.id}`}>{t.description}</Link>
-              <Link className="" to={`/users/${t.assigneeId}`}>{getUsernameFromId(t.assigneeId)}</Link>
-              <Link to={`/tickets/${t.id}/assign`} className="">Assign</Link>
-            </li>
-          ))}
+          {ticketsRenderer()}
         </ul>
       ) : (
         <span>...</span>
